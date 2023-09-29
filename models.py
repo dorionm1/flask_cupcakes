6135d7904b5c3fdde3d45333d7b4ee3b83d9766c
Dorion Mitchell <dorionmitchell@Dorions-MBP.local>
2023-09-28 21:21:45 -0400

    created cupcake model

--- models.py
+++ models.py
@@ -4,6 +4,20 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+class Cupcakes(db.Model):
+    """Cupcakes."""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(
+        db.Text, nullable=False, default="https://tinyurl.com/demo-cupcake"
+    )
+
+
 def connect_db(app):
     """Connect to database."""
 
